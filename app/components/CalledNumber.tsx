c2378256744722747082104bed15d43590afef5c
bellapacx <bellapacx@gmail.com>
2025-09-25 00:00:21 +0300

    secondsssssssssssssjahsgssafsdssfssfssssssfsdddsfddd

--- app/components/CalledNumber.tsx
+++ app/components/CalledNumber.tsx
@@ -21,7 +21,7 @@ const CalledNumber: React.FC<CalledNumberProps> = ({ numbers }) => {
       : "O";
 
   return (
-    <div className="text-3xl sm:text-4xl font-bold text-center px-4 py-2 rounded-lg bg-gradient-to-r from-yellow-400 to-yellow-300 text-black shadow-[0_0_15px_rgba(255,255,0,0.7)] animate-pulse mx-auto w-fit min-w-[80px]">
+    <div className="text-3xl sm:text-2xl font-bold text-center px-4 py-2 rounded-lg bg-gradient-to-r from-yellow-400 to-yellow-300 text-black shadow-[0_0_15px_rgba(255,255,0,0.7)] animate-pulse mx-auto w-fit min-w-[80px]">
       {letter}{latest}
     </div>
   );
