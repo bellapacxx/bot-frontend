596f2be20fd6a63e0f36e870090acebe6d62cc2c
bellapacx <bellapacx@gmail.com>
2025-09-27 10:55:45 +0300

    kdfskfkkskdfhddddsseesdfeessdsdkkkk

--- app/components/WinningsDisplay.tsx
+++ app/components/WinningsDisplay.tsx
@@ -6,7 +6,7 @@ const WinningsDisplay: React.FC = () => {
   const { potentialWinnings } = useLobby();
 
   return (
-    <div className="bg-gradient-to-r from-green-500 to-green-400 text-center  mt-1 text-black px-4 py-1 font-bold rounded shadow-sm z-50">
+    <div className="bg-gradient-to-r from-green-500 to-green-400 text-center  mt-1 text-black px-1 py-1 font-bold rounded shadow-sm z-50">
       ደራሽ: {potentialWinnings?.toFixed(2)}
     </div>
   );
